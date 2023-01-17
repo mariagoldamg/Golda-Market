26f0bb277f48a21f0aab43be00252d921623bf6f
mariagoldamg <mariamelnikova88@gmail.com>
2023-01-17 14:02:03 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+AOS.init();
+
+gsap.from ("li",{
+    y:-400,
+    delay:.3,
+    duration:2,
+ 
+})
+gsap.from (".navbar-brand",{
+    y:-400,
+    delay:.3,
+    duration:2,
+})
+
+gsap.from ("#myCarousel",{
+    x:-400,
+    delay:1,
+    duration:3,
+    opacity:0
+
+})
